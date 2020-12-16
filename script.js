9b1b7693c09f5906ab9d5b8f96b07b00b97052f7
Matthew Eggleston <MW_Eggleston@msn.com>
2020-12-16 15:52:52 -0500

    Testing initial HTML functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+var 
+
+function creatPassword() {
+    var uppercaseArray = []
+    var lowercaseArray = []
+    var numbArray = []
+    var characterArray = []
+
+
+
+    uppercaseArray [1]
+
+        var numCharacter = prompt("How many characters long would you like for your password to be?");
+        var numbers = confirm("Would you like your password to include numbers?");
+        var uppercases = confirm("How about uppercase characters?");
+        var lowercases = confirm("And lowercase characters?");
+        var characters = confirm("Last question; would you like to include non-alphanumeric characters?");
+
+
+        if(numbers){
+            resultArray = resultArray.contact(numbArray);
+        }
+
+        if(numbers){
+            resultArray = resultArray.contact(numbArray);
+        }
+
+        if(numbers){
+            resultArray = resultArray.contact(numbArray);
+        }
+        
+        if(numbers){
+            resultArray = resultArray.contact(numbArray);
+        }
+
+}
+
+}
